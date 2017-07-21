33cfb70863114d58ca58b8f8603bedee68a64139
Adam Powers <apowers@ato.ms>
2017-07-21 13:58:52 -0700

    partially working version

--- Makefile
+++ Makefile
@@ -1,14 +1,22 @@
 INCLS=-Isrc/libu2ftest/HID
 LIBPATH=-Lsrc/libu2ftest/HID
-LIBS=-lu2ftest
+# LIBS=-force_load -lsrc/libu2ftest/HID/libu2ftest.a
+LIBS += -framework CoreFoundation
+LIBS += -framework IOKit
+LIBS += -force_load src/libu2ftest/HID/*.o
+#gcc -dynamiclib -install_name
 
-.PHONY: lib/u2f-lib.dylib
+.PHONY: lib/u2f-lib.dylib deplib
 all: lib/u2f-lib.dylib
 
-lib/u2f-lib.dylib: src/u2f-lib.c Makefile
-	gcc -dynamiclib -fPIC -o lib/u2f-lib.dylib $(INCLS) $(LIBPATH) $(LIBS) src/u2f-lib.c
-	install_name_tool -change u2f-lib.0.dylib src/cbor/lib/u2f-lib.0.dylib lib/u2f-lib.dylib
+lib/u2f-lib.dylib: src/u2f-lib.c Makefile deplib
+	g++ -dynamiclib -fPIC -shared $(INCLS) $(LIBS) -o lib/u2f-lib.dylib
+	nm -gU lib/u2f-lib.dylib
+#	g++ -dynamiclib -fPIC -shared $(INCLS) $(LIBPATH) $(LIBS) -o lib/u2f-lib.dylib
+#	ld -dynamic $(LIBPATH) -lgcc_s.1 -arch x86_64 -macosx_version_min 10.12 -framework CoreFoundation -framework IOKit -all_load src/libu2ftest/HID/libu2ftest.a -o lib/u2f-lib.dylib
+#	gcc -dynamiclib -fPIC -o lib/u2f-lib.dylib $(INCLS) $(LIBPATH) $(LIBS) src/u2f-lib.c
+#	install_name_tool -change u2f-lib.0.dylib src/cbor/lib/u2f-lib.0.dylib lib/u2f-lib.dylib
 
-src/libu2ftest/HID/libu2ftest.a: Makefile
+deplib:
 	cd src/libu2ftest/HID && \
 	make
--- test/mainTest.js
+++ test/mainTest.js
@@ -3,10 +3,86 @@ var assert = require ("chai").assert;
 
 
 describe ("main test", function() {
+    var u;
     it("does init", function() {
         // this.timeout(5000);
-        var u = new U2FLib("./lib/u2f-lib.dylib");
+        u = new U2FLib("./lib/u2f-lib.dylib");
         assert.isFunction(u.init);
-        assert(u.initDone);
+        assert.isTrue(u.initDone);
+    });
+
+    it("does U2Fob_create", function() {
+        // this.timeout(5000);
+        assert.isFunction(u.U2Fob_create);
+        u.U2Fob_create();
+    });
+
+    it.skip("does U2Fob_open", function() {
+        // this.timeout(5000);
+        u = new U2FLib("./lib/u2f-lib.dylib");
+        assert.isFunction(u.init);
+        assert.isTrue(u.initDone);
+
+        assert.isFunction(u.U2Fob_init);
+        var dev = u.U2Fob_create();
+        var ret = u.U2Fob_open(dev, "IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS05@14200000/Yubikey NEO OTP+U2F+CCID@14200000/IOUSBHostInterface@1/IOUSBHostHIDDevice@14200000,1");
+        assert.strictEqual(ret, 0);
+    });
+
+    it.skip("does U2Fob_init", function() {
+        // this.timeout(5000);
+        u = new U2FLib("./lib/u2f-lib.dylib");
+        assert.isFunction(u.init);
+        assert.isTrue(u.initDone);
+
+        assert.isFunction(u.U2Fob_init);
+        var dev = u.U2Fob_create();
+        var ret = u.U2Fob_init(dev);
+        assert.strictEqual(ret, 0);
+    });
+
+    it.skip("test_Idle", function() {
+        // this.timeout(5000);
+        u = new U2FLib("./lib/u2f-lib.dylib");
+        assert.isFunction(u.init);
+        assert.isTrue(u.initDone);
+
+        assert.isFunction(u.U2Fob_init);
+        var dev = u.U2Fob_create();
+        var ret = u.U2Fob_open(dev, "IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS05@14200000/Yubikey NEO OTP+U2F+CCID@14200000/IOUSBHostInterface@1/IOUSBHostHIDDevice@14200000,1");
+        assert.strictEqual(ret, 0);
+
+        // var buf = u.getBuf();
+        var buf = new Buffer(1024);
+
+        ret = u.U2Fob_receiveHidFrame(dev, buf, 0.3);
+        assert.strictEqual(ret, -5);
+    });
+
+    it("test_Init", function() {
+        // this.timeout(5000);
+        u = new U2FLib("./lib/u2f-lib.dylib");
+        assert.isFunction(u.init);
+        assert.isTrue(u.initDone);
+
+        assert.isFunction(u.U2Fob_init);
+        var dev = u.U2Fob_create();
+        var ret = u.U2Fob_open(dev, "IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS05@14200000/Yubikey NEO OTP+U2F+CCID@14200000/IOUSBHostInterface@1/IOUSBHostHIDDevice@14200000,1");
+        assert.strictEqual(ret, 0);
+
+        // var buf = u.getBuf();
+        var buf = new Buffer(1024);
+
+        const TYPE_INIT = 0x80;
+        const U2FHID_INIT = (TYPE_INIT | 6);
+        const INIT_NONCE_SIZE = 8;
+
+        var f = u.makeFrame(-1, U2FHID_INIT, INIT_NONCE_SIZE, null);
+        console.log ("new frame:", f);
+
+        // send
+        u.U2Fob_sendHidFrame(dev, f);
+        ret = u.U2Fob_receiveHidFrame(dev, buf, 1.0);
+        assert.strictEqual(ret, 0);
     });
 });
